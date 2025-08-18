f361e5d47e25500c33214292046d8ea0465a6a4b
Joao Silva <srick8552@gmail.com>
2025-08-18 15:24:58 -0300

    comentario

--- codigo_wall-e..cpp
+++ codigo_wall-e..cpp
@@ -1,101 +1,91 @@
 #include <Ultrasonic.h>
-  #include <DMPH.h>
-  #include <Servo.h>
+#include <DMPH.h>
+#include <Servo.h>
 
-  int resultado = 0;
-  Servo servo;
-  int distanciadireita = 0;
-  int distanciaesquerda = 0;
-  Ultrasonic sensor(11, 12);
-  long distancia;
+#define DISTANCIA_SEGURA 20
 
- DMPH motor1(9, 8, 7);
- DMPH motor2(5, 4, 7);
+Servo servo;
+int resultado = 0;
+int distanciadireita = 0;
+int distanciaesquerda = 0;
+Ultrasonic sensor(11, 12);
+long distancia;
+DMPH motor1(9, 8, 7);
+DMPH motor2(10, 13 , 7);
 
- void setup(){  
-  Serial.begin (9600);
+void setup(){  
+  Serial.begin(9600);
   servo.attach(7, 500, 2500);
-  servo.write(90);
- }
+  centralizar_servo();
+}
 
- int checar (){
- 
-  servo.write(0);
-  //distancia = ??
-  delay(2000);
-  distanciadireita = sensor.read(CM);
+void centralizar_servo() {
+  servo.write(105);
+  delay(300);
+}
+
+// Checa distâncias nos lados e retorna 1 (direita) ou 2 (esquerda)
+int checar() {
+  servo.write(15);
+  delay(1000);
+  distanciadireita = sensor.Ranging(CM);
+  Serial.print("Direita: ");
   Serial.println(distanciadireita);
 
-  servo.write(180);
-  //distancia =
-  delay(2000);
-  distanciaesquerda = sensor.read(CM);
+  servo.write(165);
+  delay(1000);
+  distanciaesquerda = sensor.Ranging(CM);
+  Serial.print("Esquerda: ");
   Serial.println(distanciaesquerda);
 
-  servo.write(90);
-  if(distanciadireita > distanciaesquerda) {
-  int resultado = 1;
-  return resultado;
-  }
-  else {
-  int resultado = 2;
-  return resultado;
-  }
+  centralizar_servo();
 
- 
+  if (distanciadireita > distanciaesquerda)
+    return 1;
+  else if (distanciaesquerda > distanciadireita)
+    return 2;
+  else
+    return random(1, 3);
 }
 
-int virar_direita(){
+void virar_direita() {
   motor1.move(1);
   motor2.move(-1);
-  delay(250);
+  delay(320);
 }
 
-int virar_esquerda(){
+void virar_esquerda() {
   motor1.move(-1);
   motor2.move(1);
-  Serial.println(distanciaesquerda);
-  delay(250);
+  delay(280);
 }
 
-int parar(){
+void parar() {
   motor1.move(0);
   motor2.move(0);
   delay(1000);
 }
 
-int andar(){
+void andar() {
   motor1.move(1);
   motor2.move(1);
 }
 
+void loop() {
+  distancia = sensor.Ranging(CM);
+  Serial.print("Distância = ");
+  Serial.print(distancia);
+  Serial.println(" cm");
 
-  void loop() {
-
-  distancia = sensor.read(CM);
- Serial.print("Distância = ");
- Serial.print(distancia);
- Serial.println(" cm");
-
-
-  if (distancia>=20) {
-     andar();
-  }
-  else {
+  if (distancia >= DISTANCIA_SEGURA) {
+    andar();
+  } else {
     parar();
     resultado = checar();
-    if (resultado == 1){
+    if (resultado == 1) {
       virar_direita();
-      Serial.println(resultado);
-      delay(100);
-
-    }
-    else if (resultado == 2){
+    } else if (resultado == 2) {
       virar_esquerda();
-      Serial.println(resultado);
-      delay(100);
-
     }
-
   }
-  }
+}
